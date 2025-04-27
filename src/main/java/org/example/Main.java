1d57d7f2b314997437d039192a0ffc4b728c4035
SousaaArthur <arthursousaa2004@gmail.com>
2025-04-27 16:32:23 -0300

    feat: implement data extraction for products, categories, and prices

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -12,35 +13,68 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        String title;
-        String brand;
-        List<String> categories = new ArrayList<>();
-        String description;
-
         try {
+            String title;
+            String brand;
+            List<String> categories = new ArrayList<>();
+            String description;
+
+            List<Products> products = new ArrayList<>();
+            String name;
+            Float current_price;
+            Float old_price;
+            boolean available = true;
+
+
             Document doc = Jsoup.connect("https://infosimples.com/vagas/desafio/commercia/product.html")
                     .timeout(0)
                     .get();
 
             title = doc.select("#product_title").text();
             brand = doc.select(".brand").text();
-
-            Elements quantCategories = doc.select(".current-category > a");
-
             description = doc.select(".proddet > p").text();
 
+            Elements quantCategories = doc.select(".current-category > a");
+            Elements cardContainer = doc.select(".card-container");
 
             for (int i = 0; i < quantCategories.size(); i++){
                 String categoria = quantCategories.get(i).text();
                 categories.add(categoria);
-                System.out.println(categoria);
             }
 
-                System.out.println(
-                   "Titulo: " + title +
-                   "\nMarca: " + brand +
-                   "\nDescrição: " + description
-            );
+            for (Element card : cardContainer){
+                name = card.select(".prod-nome").text();
+                String txtCurrentPrice = card.select(".prod-pnow").text();
+                String txtOldPrice = card.select(".prod-pold").text();
+
+                if(!txtCurrentPrice.trim().isEmpty()){
+                    current_price = Float.parseFloat(txtCurrentPrice
+                            .replace("R$", "")
+                            .replace(",", ".")
+                    );
+                } else {
+                    current_price = null;
+                }
+
+                if(!txtOldPrice.trim().isEmpty()){
+                    old_price = Float.parseFloat(txtOldPrice
+                            .replace("R$","")
+                            .replace(",", ".")
+                    );
+                } else {
+                    old_price = null;
+                }
+
+                if(card.parent().hasClass("not-avaliable")){
+                    available = false;
+                }  else {
+                    available = true;
+                }
+
+                products.add(new Products(name, current_price, old_price, available));
+            }
+
+            System.out.println("Informações extraidas com sucesso ✅");
 
         } catch (IOException e){
             e.printStackTrace();
--- src/main/java/org/example/Products.java
+++ src/main/java/org/example/Products.java
@@ -2,14 +2,31 @@ package org.example;
 
 public class Products {
     private String name;
-    private float current_price;
-    private float old_price;
+    private Float current_price;
+    private Float old_price;
     private boolean available;
 
-    public Products(String name, float current_price, float old_price, boolean available) {
+    public Products(String name, Float current_price, Float old_price, boolean available) {
         this.name = name;
         this.current_price = current_price;
         this.old_price = old_price;
         this.available = available;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public Float getCurrent_price() {
+        return current_price;
+    }
+
+    public Float getOld_price() {
+        return old_price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
 }
